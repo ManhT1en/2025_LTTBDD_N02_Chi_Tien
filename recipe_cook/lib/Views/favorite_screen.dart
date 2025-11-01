ea20f01ee1d4419e257d089e240cba4ea2e4231a
Chi <hoangthilinhchi131@gmail.com>
2025-11-01 21:00:04 +0700

    fix favorite dish name dark mode

--- recipe_cook/lib/Views/favorite_screen.dart
+++ recipe_cook/lib/Views/favorite_screen.dart
@@ -9,22 +9,32 @@ class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
   @override
-  State<FavoriteScreen> createState() => _FavoriteScreenState();
+  State<FavoriteScreen> createState() =>
+      _FavoriteScreenState();
 }
 
-class _FavoriteScreenState extends State<FavoriteScreen> {
+class _FavoriteScreenState
+    extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
     final provider = FavoriteProvider.of(context);
     final favoriteItems = provider.favorites;
-    final localizations = AppLocalizations.of(context);
+    final localizations = AppLocalizations.of(
+      context,
+    );
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: Theme.of(
+        context,
+      ).scaffoldBackgroundColor,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        backgroundColor: Theme.of(
+          context,
+        ).scaffoldBackgroundColor,
         title: Text(
           localizations.favoriteRecipes,
-          style: const TextStyle(fontWeight: FontWeight.bold),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
       body: favoriteItems.isEmpty
@@ -40,34 +50,59 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
           : ListView.builder(
               itemCount: favoriteItems.length,
               itemBuilder: (context, index) {
-                String favorite = favoriteItems[index];
+                String favorite =
+                    favoriteItems[index];
                 return FutureBuilder(
-                  future: FirebaseFirestore.instance
-                      .collection('Recipe-cook-app')
+                  future: FirebaseFirestore
+                      .instance
+                      .collection(
+                        'Recipe-cook-app',
+                      )
                       .doc(favorite)
                       .get(),
                   builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
+                    if (snapshot
+                            .connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                        child:
+                            CircularProgressIndicator(),
+                      );
                     }
-                    if (!snapshot.hasData || snapshot.data == null) {
+                    if (!snapshot.hasData ||
+                        snapshot.data == null) {
                       return Center(
                         child: Text(
-                          localizations.translate('recipe_not_exist'),
+                          localizations.translate(
+                            'recipe_not_exist',
+                          ),
                         ),
                       );
                     }
-                    var favoriteItem = snapshot.data!;
+                    var favoriteItem =
+                        snapshot.data!;
                     return Stack(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.all(15),
+                          padding:
+                              const EdgeInsets.all(
+                                15,
+                              ),
                           child: Container(
-                            padding: const EdgeInsets.all(10),
-                            width: double.infinity,
+                            padding:
+                                const EdgeInsets.all(
+                                  10,
+                                ),
+                            width:
+                                double.infinity,
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.circular(
+                                    20,
+                                  ),
+                              color: Theme.of(
+                                context,
+                              ).cardColor,
                             ),
                             child: Row(
                               children: [
@@ -75,68 +110,111 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
                                   width: 100,
                                   height: 80,
                                   decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(20),
+                                    color: Theme.of(
+                                      context,
+                                    ).cardColor,
+                                    borderRadius:
+                                        BorderRadius.circular(
+                                          20,
+                                        ),
                                     image: DecorationImage(
-                                      fit: BoxFit.cover,
+                                      fit: BoxFit
+                                          .cover,
                                       image: NetworkImage(
                                         favoriteItem['image'],
                                       ),
                                     ),
                                   ),
                                 ),
-                                const SizedBox(width: 10),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      favoriteItem['name'],
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16,
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment
+                                            .start,
+                                    children: [
+                                      Text(
+                                        favoriteItem['name'],
+                                        style: TextStyle(
+                                          fontWeight:
+                                              FontWeight.bold,
+                                          fontSize:
+                                              16,
+                                          color: Theme.of(
+                                            context,
+                                          ).textTheme.bodyLarge?.color,
+                                        ),
                                       ),
-                                    ),
-                                    SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Iconsax.flash_1,
-                                          size: 16,
-                                          color: Colors.grey,
-                                        ),
-                                        Text(
-                                          ' ${favoriteItem['calo']} Calo',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                            color: Colors.grey,
+                                      SizedBox(
+                                        height:
+                                            15,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Iconsax
+                                                .flash_1,
+                                            size:
+                                                16,
+                                            color: Theme.of(
+                                              context,
+                                            ).hintColor,
                                           ),
-                                        ),
-                                        Text(
-                                          " • ",
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                            color: Colors.grey,
+                                          Text(
+                                            ' ${favoriteItem['calo']} Calo',
+                                            style: TextStyle(
+                                              fontWeight:
+                                                  FontWeight.bold,
+                                              fontSize:
+                                                  12,
+                                              color: Theme.of(
+                                                context,
+                                              ).hintColor,
+                                            ),
                                           ),
-                                        ),
-                                        Icon(
-                                          Iconsax.clock,
-                                          size: 16,
-                                          color: Colors.grey,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          ' ${favoriteItem['time']} Min',
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                            color: Colors.grey,
+                                          Text(
+                                            " • ",
+                                            style: TextStyle(
+                                              fontWeight:
+                                                  FontWeight.bold,
+                                              fontSize:
+                                                  12,
+                                              color: Theme.of(
+                                                context,
+                                              ).hintColor,
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                          Icon(
+                                            Iconsax
+                                                .clock,
+                                            size:
+                                                16,
+                                            color: Theme.of(
+                                              context,
+                                            ).hintColor,
+                                          ),
+                                          const SizedBox(
+                                            width:
+                                                5,
+                                          ),
+                                          Text(
+                                            ' ${favoriteItem['time']} Min',
+                                            style: TextStyle(
+                                              fontWeight:
+                                                  FontWeight.bold,
+                                              fontSize:
+                                                  12,
+                                              color: Theme.of(
+                                                context,
+                                              ).hintColor,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
@@ -149,7 +227,11 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
                           child: GestureDetector(
                             onTap: () {
                               setState(() {
-                                provider.toggleFavoriteStatus(snapshot.data!);
+                                provider
+                                    .toggleFavoriteStatus(
+                                      snapshot
+                                          .data!,
+                                    );
                               });
                             },
                             child: const Icon(
